07e4e67eda97f06199f8c2c90bfd5bcc811a67b3
yungkeong21-pixel <yungkeong21@gmail.com>
2026-07-23 01:14:43 +0800

    Restore cache version after deferred app update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pawtrack-v17';
+const CACHE='pawtrack-v16';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','index.html','manifest.webmanifest']))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))));
